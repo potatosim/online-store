f38ea6f4e6bd8361745cb554c9d995a66f9b6ce1
Leontiy Knyazev <leontiy.knyazev@gmail.com>
2022-12-20 21:59:13 +0600

    feat: add and styled header component

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './Header.module.scss';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Badge } from '@mui/material';
+import logo from './../../assets/logo.png';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img className={styles.logo__img} src={logo} />
+        <h2 className={styles.logo__text}>Online Store</h2>
+      </div>
+      <p className={styles.total}>
+        <span className={styles.total__text}>Cart total: </span>
+        <span className={styles.total__price}>€{'0.00'}</span>
+      </p>
+      <Badge badgeContent={0} color="warning" overlap="circular" showZero>
+        <ShoppingCartIcon
+          sx={{
+            color: 'orange',
+            width: 45,
+            height: 45,
+            cursor: 'pointer',
+          }}
+        />
+      </Badge>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import { Box } from '@mui/material';
+
+import Footer from './Footer';
+import Header from './Header';
+
+const LayoutWrapper = styled(Box)({});
+
+const TopContentWrapper = styled(Box)({});
 
 const Layout = () => {
-  return <Outlet />;
+  return (
+    <LayoutWrapper>
+      <TopContentWrapper>
+        <Header />
+
+        <Outlet />
+
+        <Footer />
+      </TopContentWrapper>
+    </LayoutWrapper>
+  );
 };
 
 export default Layout;
