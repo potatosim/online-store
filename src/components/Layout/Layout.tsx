dc0b6d3f603ed9bc92b8b6814590e9762534da7f
Leontiy Knyazev <leontiy.knyazev@gmail.com>
2022-12-20 23:03:49 +0600

    feat: add main, variables and styles for layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import { styled } from '@mui/material/styles';
 import { Box } from '@mui/material';
-
 import Footer from './Footer';
 import Header from './Header';
+import Main from './Main';
+import { Outlet } from 'react-router-dom';
+import React from 'react';
+import styled from '@emotion/styled';
 
-const LayoutWrapper = styled(Box)({});
+const LayoutWrapper = styled(Box)(() => ({
+  width: '100%',
+  minHeight: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'stretch',
+}));
 
-const TopContentWrapper = styled(Box)({});
+const TopContentWrapper = styled(Box)`
+  display: flex;
+  flex: 1 1 auto;
+  flex-direction: column;
+  overflow: hidden;
+`;
 
 const Layout = () => {
   return (
     <LayoutWrapper>
       <TopContentWrapper>
         <Header />
-
-        <Outlet />
-
-        <Footer />
+        <Main>
+          <Outlet />
+        </Main>
       </TopContentWrapper>
+      <Footer />
     </LayoutWrapper>
   );
 };
