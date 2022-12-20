3252a9fe3b8edc12bfacafad5e2b9eb4fa0cc312
Anna Yemelyanova <devildriver2310@gmail.com>
2022-12-20 20:29:39 +0300

    fix: fix notFoundPage styles according to main layout

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import { Button } from '@mui/material';
+import SentimentVeryDissatisfiedOutlinedIcon from '@mui/icons-material/SentimentVeryDissatisfiedOutlined';
+import { useNavigate } from 'react-router-dom';
+import styles from './NotFoundPage.module.scss';
+
+const NotFoundPage = () => {
+  const navigate = useNavigate();
+  return (
+    <div className={styles.pageWrapper}>
+      <div className={styles.typographyWrapper}>
+        <Typography
+          fontWeight={500}
+          fontSize="10rem"
+          variant="h1"
+          color="#9c27b0"
+          sx={{
+            background: 'linear-gradient(to right,#f2580e 0,#f2ca0f 50%,#f2580e 100%)',
+            backgroundClip: 'text',
+            webkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+          }}
+        >
+          404
+        </Typography>
+        <Typography
+          variant="h2"
+          fontSize="5rem"
+          fontWeight={500}
+          sx={{
+            color: 'rgb(0 0 0 / 80%)',
+          }}
+        >
+          Page n
+          <SentimentVeryDissatisfiedOutlinedIcon style={{ minWidth: '3rem', minHeight: '3rem' }} />t
+          found
+        </Typography>
+      </div>
+      <Button
+        onClick={() => navigate('/')}
+        variant="text"
+        color="warning"
+        size="large"
+        sx={{
+          fontSize: '1.5rem',
+          ':hover': {
+            backgroundColor: 'rgb(242, 88, 14, 0.2)',
+          },
+        }}
+      >
+        Back to the store
+      </Button>
+    </div>
+  );
+};
+
+export default NotFoundPage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import Layout from 'components/Layout';
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
     </Routes>
   );
 };
