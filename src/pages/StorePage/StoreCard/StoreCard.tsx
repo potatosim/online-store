3f209145d7ac2ddd1d9953d11535efaa172cfd1a
Leontiy Knyazev <leontiy.knyazev@gmail.com>
2023-01-06 16:06:30 +0600

    feat: add template for store page cards and minor fixes for store page

--- src/pages/StorePage/StoreCard/StoreCard.tsx
+++ src/pages/StorePage/StoreCard/StoreCard.tsx
@@ -1,6 +1,8 @@
-import { Button, Card, CardActions, CardMedia } from '@mui/material';
-import CardContent from '@mui/material/CardContent';
+import styled from '@emotion/styled';
+import { Button, Card, CardActions, CardContent, CardMedia, Rating, Typography } from '@mui/material';
 import Paper from '@mui/material/Paper/Paper';
+import currencyFormatter from 'helpers/currencyFormatter';
+import percentageFormatter from 'helpers/percentageFormatter';
 import React, { FC } from 'react';
 import { ProductItem } from 'types/ProductItem';
 
@@ -15,15 +17,68 @@ interface StoreItemProps {
 
 const StoreCard: FC<StoreItemProps> = ({ storeCardItem }) => {
   const { brand, category, discountPercentage, price, thumbnail, rating, stock } = storeCardItem;
+
+  const Text = styled(Typography)`
+    margin: 0;
+    font-size: 14px;
+  `;
+
+  const Span = styled('span')`
+    font-weight: 700;
+  `;
+
   return (
-    <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <Card
+      variant="outlined"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '250px',
+        height: '350px',
+        boxShadow: '2px 2px 5px grey',
+      }}
+    >
       <Paper elevation={12} className={styles.imageWrapper}>
         <CardMedia className={styles.imageWrapper} image={thumbnail} />
       </Paper>
-      <CardContent>{brand + category + discountPercentage + price + rating + stock}</CardContent>
-      <CardActions>
-        <Button>Add to cart</Button>
-        <Button>Details</Button>
+      <CardContent
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'start',
+          width: '100%',
+          padding: '15px 15px 10px',
+        }}
+      >
+        <Text>
+          <Span>Category:</Span> {category}
+        </Text>
+        <Text>
+          <Span>Brand:</Span> {brand}
+        </Text>
+        <Text>
+          <Span>Price:</Span> {currencyFormatter.format(price)}
+        </Text>
+        <Text>
+          <Span>Discount:</Span>{' '}
+          {percentageFormatter.format(discountPercentage / 100)}
+        </Text>
+        <Text sx={{ display: 'flex', alignItems: 'center' }}>
+          <Span>Rating: </Span>{' '}
+          <Rating name="read-only" max={5} value={rating} precision={0.5} readOnly size="small" />
+        </Text>
+        <Text>
+          <Span>Stock:</Span> {stock}
+        </Text>
+      </CardContent>
+      <CardActions
+        sx={{
+          padding: '0',
+        }}
+      >
+        <Button variant="outlined">Add to cart</Button>
+        <Button variant="outlined">Details</Button>
       </CardActions>
     </Card>
   );
